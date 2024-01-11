2cb200a7ed71e1917ea2ee56a9370d2459cec0fd
Manh Tu <xxlaguna93@gmail.com>
2024-01-11 18:07:46 +0800

    response +PONG\r\n

--- app/server.go
+++ app/server.go
@@ -3,18 +3,37 @@ package main
 import (
 	"fmt"
 	"net"
-	"os"
 )
 
 func main() {
 	l, err := net.Listen("tcp", "0.0.0.0:6379")
-	if err != nil {
-		fmt.Println("Failed to bind to port 6379")
-		os.Exit(1)
+	must(err)
+	defer l.Close()
+
+	fmt.Println("Redis Server is running on port 6379...")
+	for {
+		conn, err := l.Accept()
+		if err != nil {
+			fmt.Println("Error accepting connection:", err)
+			continue
+		}
+		go handleConn(conn)
 	}
-	_, err = l.Accept()
+}
+
+func handleConn(conn net.Conn) {
+	defer conn.Close()
+	buf := make([]byte, 1024)
+	n, err := conn.Read(buf)
+	must(err)
+	fmt.Println("Received: ", buf[:n])
+	fmt.Println("Received: ", string(buf[:n]))
+  resp := []byte("+PONG\r\n")
+  conn.Write(resp)
+}
+
+func must(err error) {
 	if err != nil {
-		fmt.Println("Error accepting connection: ", err.Error())
-		os.Exit(1)
+		fmt.Println(err)
 	}
 }
